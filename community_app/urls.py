2c9b15920697c8b4635f7683208cb90236d640c5
Mamadou Aliou Diallo <alioukahere@gmail.com>
2020-07-17 19:49:21 +0000

    Creates and design templates

--- community_app/urls.py
+++ community_app/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('members/', include('members.urls')),
+    path('', include('members.urls')),
     path('admin/', admin.site.urls),
 ]
--- members/views.py
+++ members/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def homepage(request):
-    return HttpResponse('<h1>Bienvenue sur ma communauté de devs.</h1>')
+    return render(request, 'members/index.html')
 
 
 def member_view(request, member_id):
-    return HttpResponse('<h1>Affichage du membre numéro {}</h1>'.format(member_id))
+    return render(request, 'members/member_view.html', locals())
