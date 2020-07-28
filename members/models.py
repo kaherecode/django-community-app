3b349ca1117aa64b55a467618c9d528839ac08f9
Mamadou Aliou Diallo <alioukahere@gmail.com>
2020-07-28 18:26:44 +0000

    adds member model

--- members/models.py
+++ members/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    title = models.CharField(max_length=255)
+    bio = models.TextField(null=True, blank=True)
+    city = models.CharField(max_length=255)
+    github = models.CharField(max_length=255, null=True, blank=True)
+    twitter = models.CharField(max_length=255, null=True, blank=True)
+    linkedin = models.CharField(max_length=255, null=True, blank=True)
+    facebook = models.CharField(max_length=255, null=True, blank=True)
+    instagram = models.CharField(max_length=255, null=True, blank=True)
+    website = models.CharField(max_length=255, null=True, blank=True)
+    picture = models.FileField()
+    is_active = models.BooleanField(default=True)
+    joined_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
